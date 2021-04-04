bdf571db8cdadb929391bb5a131e60226f969c76
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2021-04-04 00:34:00 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pro Bem.Org' });
+  res.render('index', { title: 'Agape.Org' });
 });
 
 module.exports = router;
