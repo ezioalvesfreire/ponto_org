1c531ff1ff0f456f70a8d6df9541a71a5ec1de0d
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2021-04-16 02:19:26 -0300

    criada a rota e  pagina de postagens

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var postsService = require('../services/postsService');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Agape.Org' });
+});
+/* GET postagens page. */
+router.get('/photo_posts', function (req, res, next) {
+  var posts = postsService.getPosts();
+
+  res.render('photo_posts', { title: 'Agape.org', posts: posts });
 });
 
 module.exports = router;
---
+++ services/postsService.js
@@ -0,0 +1,62 @@
+var getPosts = function () {
+
+    var posts = [
+        {
+            id: 1,
+            title: 'Postagem 1',
+            image: 'caldas_country-GO-512x290.jpg',
+            description: 'Descrição da primeira postagem',
+            body: 'Descrição mais detalhada da primeira postagem'
+
+        },
+
+        {
+            id: 2,
+            title: 'Postagem 2',
+            image: 'machine_learning-512x290.jpg',
+            description: 'Descrição da segunda postagem',
+            body: 'Descrição mais detalhada da segunda postagem'
+
+        },
+        {
+            id: 3,
+            title: 'Postagem 3',
+            image: 'image512x290.jpg',
+            description: 'Descrição da terceira postagem',
+            body: 'Descrição mais detalhada da terceira postagem'
+
+        },
+        {
+            id: 4,
+            title: 'Postagem 4',
+            image: 'download-512x290.jpg',
+            description: 'Descrição da quarta postagem',
+            body: 'Descrição mais detalhada da quarta postagem'
+
+        },
+        {
+            id: 5,
+            title: 'Postagem 5',
+            image: 'img-512x290.jpg',
+            description: 'Descrição da quinta postagem',
+            body: 'Descrição mais detalhada da quinta postagem'
+
+        },
+        {
+            id: 6,
+            title: 'Postagem 6',
+            image: 'download-512x290.jpg',
+            description: 'Descrição da sexta postagem',
+            body: 'Descrição mais detalhada da sexta postagem'
+
+        },
+
+
+    ];
+
+    return posts;
+}
+
+module.exports = {
+    getPosts: getPosts
+}
